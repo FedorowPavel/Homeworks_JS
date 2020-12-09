afbdcc437f5f79d4b01b9ce2bce49f60155a79ae
FedorowPavel <pasha_fedorow@mail.ru>
2020-12-09 10:34:23 +0200

    1st task fixed

--- Homework_05/js/script.js
+++ Homework_05/js/script.js
@@ -1,7 +1,5 @@
-
 console.log('');
 console.log('Task_01');
-
 
 class Smoothie {
     //конструктор принимает в себя список ингридиентов
@@ -9,7 +7,6 @@ class Smoothie {
         //создаем состояние класса
         //присваиваем список ингридиентов в объект, который создастся из класса smothi
         this.ingredients = ingredients;
-
         //мап ингридиентов, откуда возьмется цена соответсвтующего ингридиента
         this.ingredientsMap = {
             'Strawberries': 1.5,
@@ -24,43 +21,40 @@ class Smoothie {
 
     //метод подсчета стоимости смуззи
     getCost() {
-        //без этого не работала строчка 31
+        //без этого не работала строчка 27
         const ingMap = this.ingredientsMap
-
         const cost = this.ingredients.reduce(function (result, curr) {
             const ingredientCost = ingMap[curr];
             return result + ingredientCost;
         }, 0)
-
         return `$${cost.toFixed(2)}`;
     }
 
     getName() {
         if (this.ingredients.length === 1) {
             return `${this.ingredients[0]} Smoothie`
-        } else {
-            //сортируем массив по алфавиту
-            const sortIngredients = this.ingredients.sort();
-            //делаем реверс сортированного массива
-            //так как редьюс идет с конца
-            const reverseSortedIngredients = sortIngredients.reverse();
-
-            const name = reverseSortedIngredients.reduce(function (result, curr) {
-                const resultName = curr;
-                return `${resultName} ${result}`;
-            }, `Fusion`)
-            return name;
         }
+        //копируем во временный массив весь исходный массив
+        //чтобы избежать мутирования начального массива
+        const tempIngredients = [...this.ingredients];
+        //сортируем массив по алфавиту
+        const sortIngredients = tempIngredients.sort();
+        //делаем реверс сортированного массива
+        //так как редьюс идет с конца
+        const reverseSortedIngredients = sortIngredients.reverse();
+        const name = reverseSortedIngredients.reduce(function (result, curr) {
+            return `${curr} ${result}`;
+        }, `Fusion`)
+        return name;
     }
 }
 
 const newSmothie = new Smoothie(['Raspberries', 'Strawberries', 'Blueberries']);
 
 console.log(newSmothie.ingredients);
-// console.log(newSmothie.ingredientsMap);
-// console.log(newSmothie.ingredientMap['Strawberries']);
 console.log(newSmothie.getCost());
 console.log(newSmothie.getName());
+console.log(newSmothie.ingredients);
 
 
 
@@ -71,20 +65,23 @@ console.log('Task_02');
 class GameOfWords {
 
     constructor() {
-        this.listOfWords = [];
+        this.words = [];
+        this.gameOver;
     }
 
     play(word) {
-        if (this.listOfWords.length === 0) {
-            return [...this.listOfWords, word];
-        } else {
-            this.listOfWords = [...this.listOfWords, word];
-            return this.listOfWords;
+        //случай для первого вызова метода play
+        //просто копирем первое слово в список слов
+
+        if (this.words.length === 0) {
+            return [...this.words, word];
         }
 
     }
 
+    restart() {
 
+    }
 }
 
 const game = new GameOfWords();
@@ -92,6 +89,6 @@ const game = new GameOfWords();
 console.log(game);
 
 console.log(game.play('ball'));
-console.log(game);
-console.log(game.play('long'));
+// console.log(game);
+console.log(game.play('wall'));
 // console.log(game);
