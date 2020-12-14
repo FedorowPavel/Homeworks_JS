d96a9c8cbbf92913e12255e53c77a8ace4104813
FedorowPavel <pasha_fedorow@mail.ru>
2020-12-14 10:36:35 +0200

    2nd task added

--- Homework-06/js/script.js
+++ Homework-06/js/script.js
@@ -93,20 +93,29 @@ const scheme = ['A1', 'D1', 'E1', 'B3', 'E3', 'B4', 'D5'];
 
 class Battleship {
     constructor(boardScheme) {
+        //добавляем введенные координаты кораблей в наш объект
         this.boardScheme = boardScheme;
+        //отрисовываем доску с расположением кораблей
         this.board = this.getBoard();
     }
 
     getBoard() {
+        //создаем внешний массив из 5 элементов
         return Array(5).fill(0).map((_, i) => {
+            //каждый из внутернних элементов заполняем массивом из 5 элементов
             return Array(5).fill(0).map((_, j) => {
+                //присваиваем буквенную часть координаты элемента
+                //получаем из позиции циферной позицию буквенную
                 const letter = String.fromCharCode(j + 65);
+                //записываем итоговые координаты (буква, число)
                 const coordinate = `${letter}${i + 1}`;
 
+                //если в схеме расположени кораблей есть такая координата
+                //то ставим на это место корабль (ставим 1)
                 if (scheme.includes(coordinate)) {
                     return 1;
                 }
-
+                //иначе ставим 0
                 return 0;
             });
         })
@@ -117,42 +126,72 @@ class Battleship {
     }
 
     turn(coordinate) {
+        //проверяем условие, есть ли введенная коордната 
+        //в списке координат кораблей
         if (this.boardScheme.includes(coordinate)) {
+            //если есть то
+            //отфильтруем массив координат кораблей
+            //по значению введенному в координаты
+            //то есть оставим только это значение
             this.boardScheme = this.boardScheme.filter((item) => item !== coordinate);
 
+            //переведем координаты в численные значения
             const letter = coordinate[0].charCodeAt(0) - 65;
             const index = parseInt(coordinate[1], 10) - 1;
 
+            //заменяем значение в данной ячейке на -1 (было 1)
             this.board[index][letter] = -1;
 
+            //проводим проверки одиночный или двойной корабль
+
+            //если знчение справа истинно (не равно 0) то
             if (this.board[index][letter + 1]) {
+                //и если это значине равно -1 (раненый корабль)
                 if (this.board[index][letter + 1] === -1) {
+                    //то убили
                     return 'killed';
                 }
+                //если не равно -1 то ранили (значит значение 1)
                 return 'unjured';
             }
 
+            //если знчение слева истинно (не равно 0) то
             if (this.board[index][letter - 1]) {
+                //и если это значине равно -1 (раненый корабль)
                 if (this.board[index][letter - 1] === -1) {
+                    //то убили
                     return 'killed';
                 }
-                return 'unjured';
-            }
-            if (this.board[index + 1][letter]) {
-                if (this.board[index + 1][letter] === -1) {
-                    return 'killed';
-                }
-                return 'unjured';
-            }
-            if (index && this.board[index - 1][letter]) {
-                if (this.board[index - 1][letter] === -1) {
-                    return 'killed';
-                }
+                //если не равно -1 то ранили (значит значение 1)
                 return 'unjured';
             }
 
+            //если знчение снизу истинно (не равно 0) то
+            //<4 отсекает для проверки последнюю строку
+            if (index < 4 && this.board[index + 1][letter]) {
+                //и если это значине равно -1 (раненый корабль)
+                if (this.board[index + 1][letter] === -1) {
+                    //то убили
+                    return 'killed';
+                }
+                //если не равно -1 то ранили (значит значение 1)
+                return 'unjured';
+            }
+
+            //если знчение сверху истинно (не равно 0) то
+            if (index && this.board[index - 1][letter]) {
+                //и если это значине равно -1 (раненый корабль)
+                if (this.board[index - 1][letter] === -1) {
+                    //то убили
+                    return 'killed';
+                }
+                //если не равно -1 то ранили (значит значение 1)
+                return 'unjured';
+            }
+            //если проверки не прошли значит корабль одиночный и убили
             return 'killed!';
         };
+        //ввели координату где нет корабля
         return 'miss!';
     }
 
@@ -162,6 +201,28 @@ class Battleship {
 const battleship = new Battleship(scheme);
 console.log(battleship.getBoard());
 console.log(battleship.turn('A1'));
-console.log(battleship.turn('D1'));
 console.log(battleship.turn('A2'));
+console.log(battleship.turn('A3'));
+console.log(battleship.turn('A4'));
+console.log(battleship.turn('A5'));
+console.log(battleship.turn('B1'));
+console.log(battleship.turn('B2'));
+console.log(battleship.turn('B3'));
+console.log(battleship.turn('B4'));
+console.log(battleship.turn('B5'));
+console.log(battleship.turn('C1'));
+console.log(battleship.turn('C2'));
+console.log(battleship.turn('C3'));
+console.log(battleship.turn('C4'));
+console.log(battleship.turn('C5'));
+console.log(battleship.turn('D1'));
+console.log(battleship.turn('D2'));
+console.log(battleship.turn('D3'));
+console.log(battleship.turn('D4'));
+console.log(battleship.turn('D5'));
+console.log(battleship.turn('E1'));
+console.log(battleship.turn('E2'));
+console.log(battleship.turn('E3'));
+console.log(battleship.turn('E4'));
+console.log(battleship.turn('E5'));
 
